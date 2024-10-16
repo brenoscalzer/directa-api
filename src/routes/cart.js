7912e44f856a44b17740e56945cac8f99f5c0cd5
Breno Scalzer <brenoscalzer1@gmail.com>
2024-10-16 01:33:49 -0300

    last improvements

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -47,4 +47,50 @@ router.post('/add-item', auth, async (req, res) => {
   }
 );
 
+router.post('/remove-item', auth, async (req, res) => {
+    const { productId } = req.body;
+    try {
+      
+      const cart = await Cart.findOne({ user: req.user.id });
+      
+      if (!cart.items.includes(productId)) {
+        res.status(400).send({
+          message: 'Item is not included in your cart'
+        });
+      } else {
+        cart.items = [...cart.items.filter(item => item !== productId)];
+
+        await cart.save();
+      
+        res.status(200).json({
+          message: 'success'
+        });
+      }
+    } catch (err) {
+      res.status(500).send({
+        message: err.message
+      });
+    }
+  }
+);
+
+router.delete('/clear-cart', auth, async (req, res) => {
+    try {
+      const cart = await Cart.findOne({ user: req.user.id });
+      
+      cart.items = [];
+
+      await cart.save();
+    
+      res.status(200).json({
+        message: 'success'
+      });
+    } catch (err) {
+      res.status(500).send({
+        message: err.message
+      });
+    }
+  }
+);
+
 module.exports = router;
